502aad38823f9210d46d58833d038a9669fe17fe
Renato Nunes <renatonunesan@gmail.com>
2024-03-05 12:19:04 -0300

    Update js folder

---
+++ var/ex.js
@@ -0,0 +1,9 @@
+var nome = "Renato";
+var idade = "21";
+
+var comidaFavorita;
+comidaFavorita = "Pizza";
+
+var timeA, timeB, timeC, timeD, timeE;
+
+console.log(nome, idade, comidaFavorita);
---
+++ var/script.js
@@ -0,0 +1,20 @@
+var nome = 'Renato';
+var idade = 150;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade)
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Renato',
+    cidade = 'Lucrecia';
+
+console.log (sobrenome, cidade);    
+
+// var time = 'Santos';
+// time = 'Flamengo'
+// console.log(time);
