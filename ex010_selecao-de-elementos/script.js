7113a209ce306c1239204ff1743aa7cef62855fb
Renato Nunes <renatonunesan@gmail.com>
2024-03-08 10:57:32 -0300

    new ex010

---
+++ ex010_selecao-de-elementos/script.js
@@ -0,0 +1,28 @@
+/*
+
+ex001
+
+const imgAnimais = document.querySelectorAll('img');
+console.log(imgAnimais);
+
+ex002
+
+const imgSelect = document.querySelectorAll('img[src^="../images/imagem"]');
+console.log(imgSelect);
+
+ex003
+
+const linksInterno = document.querySelectorAll('[href^="#"]');
+console.log(linksInterno);
+
+ex004
+
+const primeiroH2 = document.querySelector('.animais-descricao h2');
+console.log(primeiroH2);
+
+ex005
+
+const paragrafos = document.querySelectorAll('p');
+console.log(paragrafos[paragrafos.length - 1])
+
+*/
