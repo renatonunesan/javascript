c5cd87582346d98ae2e1217eb80b11a655d95248
Renato Nunes <renatonunesan@gmail.com>
2024-03-14 16:19:07 -0300

    new ex025

---
+++ ex025_date-object/script.js
@@ -0,0 +1,14 @@
+// O construtor Date cria um objeto que vai receber valores como mês, dia, horário e outros. A data é baseada na hora do 
+// relógio do computador
+
+const agora = new Date();
+agora;
+// Semana Mês Dia Ano HH:MM:SS GMT
+agora.getDate() // Dia
+agora.getDay() // Dia da Semana ex: 5 = Fri
+agora.getMonth() // Número dia mês
+agora.getFullYear() // Ano
+agora.getHours() // Hora
+agora.getMinutes() // Minutos
+agora.getTime() // ms desde 1970
+agora.getUTCHours() - 3 // Brasília
